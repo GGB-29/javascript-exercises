9d3732b8dfe0e1355645cb9c57e0e2683bfe4463
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-08-02 18:42:00 +0100

    feat: Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let letters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    let newString = string
+        .toLowerCase()
+        .split('')
+        .filter(char => letters.includes(char))
+        .join('');
+
+    let reversedString = newString
+        .split('')
+        .reverse()
+        .join('');
+
+    if (newString === reversedString) {
+        return true;
+    }
+    return false;
 
 };
 
