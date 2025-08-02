86b0bdc8483b19c84eaceae1a89836eeaad4ee25
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2025-08-02 18:35:39 +0100

    feat: Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, number) => (sum + number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((product, number) => (product * number), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1;
+  while (a > 0) {
+    product *= a;
+    a--;
+  }
+  return product;
 };
 
 // Do not edit below this line
